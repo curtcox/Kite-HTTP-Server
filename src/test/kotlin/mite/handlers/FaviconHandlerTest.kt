4b50d1161aef461d569b75b495b64bc64f24334d
Curt Cox <curt@Curts-MacBook-Pro.local>
2021-01-02 21:14:57 -0600

    Added test

---
+++ src/test/kotlin/mite/handlers/FaviconHandlerTest.kt
@@ -0,0 +1,19 @@
+package mite.handlers
+
+import mite.core.*
+import org.junit.Test
+import kotlin.test.*
+
+class FaviconHandlerTest {
+
+    val favicon = FaviconHandler
+    fun request(filename:String) = HTTPRequest("","",filename, HTTPVersion.Unknown)
+
+    @Test
+    fun `returns icon`() {
+        val response = favicon.handle(request("/favicon.ico"))
+        assertEquals(StatusCode.OK,response.status)
+        assertEquals(ContentType.ICON,response.contentType)
+        assertEquals(15406,response.bytes.size)
+    }
+}
