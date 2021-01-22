f364f75edc1ec11bcd86617d308478dd417c3576
Curt Cox <curt@Curts-MacBook-Pro.local>
2021-01-21 21:07:17 -0600

    Added test.

--- src/test/kotlin/mite/renderers/HtmlRendererTest.kt
+++ src/test/kotlin/mite/renderers/HtmlRendererTest.kt
@@ -2,8 +2,10 @@ package mite.renderers
 
 import mite.ast.Node
 import mite.core.Log
+import mite.core.Log.Entry
 import mite.http.HTTP.*
 import org.junit.Test
+import java.time.Instant
 import kotlin.test.*
 
 class HtmlRendererTest {
@@ -15,7 +17,7 @@ class HtmlRendererTest {
     )
 
 
-    fun entries(entries:List<Node>) = InternalResponse.node(Node.list(Log::class, entries),HtmlRenderer())
+    fun entries(entries:List<Entry>) = InternalResponse.node(Node.list(Log::class, entries),HtmlRenderer())
 
     @Test
     fun `no entries renders as HTML`() {
@@ -26,4 +28,25 @@ class HtmlRendererTest {
         assertTrue(page.startsWith("<HTML>"))
         assertTrue(page.endsWith("</HTML>"))
     }
+
+    @Test
+    fun `one entry renders as HTML table`() {
+        val time = Instant.now()
+        val logger = this
+        val record = "stuff we want to record"
+        val at = Throwable()
+        val response = entries(listOf(Entry(time,logger,record, at)))
+        val rendered = renderer.render(request,response)
+        assertEquals(ContentType.HTML,rendered.contentType)
+        val page = rendered.page
+        assertTrue(page.startsWith("<HTML>"))
+        assertTrue(page.endsWith("</HTML>"))
+        assertTrue(page.contains("<TABLE>"))
+        assertTrue(page.contains("</TABLE>"))
+        assertTrue(page.contains(time.toString()))
+        assertTrue(page.contains(logger.toString()))
+        assertTrue(page.contains(record))
+        assertTrue(page.contains(at.toString()))
+    }
+
 }
